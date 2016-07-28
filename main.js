a1545819d766d12e511008bbfc845860e5f9da9f
rittersm <rittersm@gmail.com>
2016-07-28 09:42:49 -0400

    missed method name

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
 
-  function bodyDisplay(objs){
-    $.getJSON('https://calm-beach-80027.herokuapp.com/timeline', objs,  function(data){
+  function pageCount(num) {
+    return Math.ceil(num / 10)
+  }
+
+  function paginationCount(pages, currentPage){
+    console.log(currentPage)
+    if($('nav').length === 0){
+      $('#timeline').prepend(
+        "<nav aria-label=\"Page navigation\"><ul class=\"pagination\"></ul></nav>"
+      )
+    }
+    $('.pagination').html("")
+    for(i=0;i<pages;i++){
+      console.log(i)
+      if((i + 1) == currentPage){
+        $('.pagination').append("<li class=\"active\"><a href=\"" + (i + 1) + "\">" + (i + 1) + "</a></li>")
+      } else {
+        $('.pagination').append("<li><a href=\"" + (i + 1) + "\">" + (i + 1) + "</a></li>")
+      }
+    }
+  }
+
+
+  function bodyDisplay(num){
+    $.getJSON('https://calm-beach-80027.herokuapp.com/timeline/', 'page=' + num,  function(data){
+      paginationCount(pageCount(data.length), num)
       timelineDisplay(data)
-      console.log(data)
     })
   }
   var source   = $("#chirp-template").html();
@@ -20,5 +43,11 @@ $(document).ready(function(){
     })
   }
 
-  bodyDisplay()
+  bodyDisplay(2)
+
+  $(document.body).on('click', '.pagination li a', function(ev){
+  ev.preventDefault()
+  timelineDisplay($(ev.target).attr('href'))
+  })
+
 })
